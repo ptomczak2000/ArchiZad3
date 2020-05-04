07c9bdd9a7081dae5893edb411bee927fd565910
ptomczak2000 <ptomczak2000@users.noreply.github.com>
2020-05-04 17:44:32 +0200

    Update Module1.asm

--- ArchiZad3/Module1.asm
+++ ArchiZad3/Module1.asm
@@ -6,14 +6,18 @@
 ; Autorzy        : Piotr Tomczak, Kacper Wiœniewski, 2, poniedzia³ek, 12:15   ;
 ; Data zaliczenia: 23.03.2020r.                                               ;
 ; Uwagi          : Plik zawieraj¹cy modu³y do obliczania sredniej tablicy	  ;			
-;                   oraz zliczaj¹cy ilosc znaku w tablicy                     ;
+;                  oraz zliczaj¹cy ilosc znaku w tablicy                      ;
 ;=============================================================================;
 				.MODEL		SMALL, C++
 				.CODE 
 				PUBLIC		tablica
 				PUBLIC		zliczenia
+
 tablica:		PROC
 				mov  ax, 0
 				
+				ENDP
+zliczenia:		PROC
+
 				ENDP
 				END
