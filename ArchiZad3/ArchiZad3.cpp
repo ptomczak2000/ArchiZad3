2cd7efe727dbf0523181ca8e38b8f707c9d91537
ptomczak2000 <ptomczak2000@users.noreply.github.com>
2020-05-04 17:28:18 +0200

    Dodaj pliki projektów.

---
+++ ArchiZad3/ArchiZad3.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+extern "C" float tablica(int n, int tab[]);
+extern "C" int zliczenia(char tab[], char znaczek);
+int main()
+{
+    int wybor;
+    char znak;
+    cout << "----Archi---ZAD3----" << endl;
+    cout << "1. Obliczanie sredniej z tablicy" << endl;
+    cout << "2. Obliczanie wystapien danego znaku w tablicy znakowej" << endl;
+    cout << "Wybierz opcje: ";
+    cin >> wybor;
+    do{
+        switch (wybor) {
+        case 1: {
+            int ilosc;
+            float srednia;
+            cout << "Podaj podaj ilosc elementow: ";
+            cin >> ilosc;
+            int* tab = new int[ilosc];
+            for (int i = 0; i < ilosc; i++) {
+                cout << "Podaj element"<< i+1 << " tablicy: ";
+                cin >> tab[i];
+            }
+            //srednia=tablica(ilosc, tab);
+            //cout << "Srednia wynosi: " << srednia << endl;
+            delete tab;
+            break;
+        }
+        case 2: {
+            char znaczek;
+            char tab[256];
+            int ilosc;
+            cout << "Podaj tablice znakow: ";
+            cin.get();
+            cin.getline(tab, 256);
+            cout << "Podaj szukany znak: ";
+            cin >> znaczek;
+            //ilosc=zliczenia(tab, znaczek);
+            break;
+        }
+        default: {
+            cout << "Wybrano zla opcje!" << endl;
+        }
+        }
+        cout << "Czy chcesz kontynuowac? [y/n]";
+        cin >> znak;
+    }while(znak=='y' || znak=='Y');
+    return 0;
+}
---
+++ ArchiZad3/Module1.asm
@@ -0,0 +1,20 @@
+;=============================================================================;
+;                                                                             ;
+; Plik           : Module1.asm                                                ;
+; Format         : EXE                                                        ;
+; Cwiczenie      : Program Hybrydowy										  ;
+; Autorzy        : Piotr Tomczak, Kacper Wiœniewski, 2, poniedzia³ek, 12:15   ;
+; Data zaliczenia: 23.03.2020r.                                               ;
+; Uwagi          : Plik zawieraj¹cy modu³y do obliczania sredniej tablicy	  ;			
+;                   oraz zliczaj¹cy ilosc znaku w tablicy                     ;
+;=============================================================================;
+				.MODEL		SMALL, C++
+				.CODE 
+				PUBLIC		tablica
+				PUBLIC		zliczenia
+				.DATA
+
+tablica:		PROC
+				mov 
+				ENDP
+				END
